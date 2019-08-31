dcd6773e5e8e5d08045426ff1f09cae28fbc8095
Alex Sanschagrin <asanscha@gmail.com>
2019-08-31 13:50:20 -0700

    added working fs, liri reads and searches random.txt file but spotify still broken

--- liri.js
+++ liri.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
+var fs = require('fs');
 
 var keys = require("./keys.js");
 
 // var spotify = new Spotify(keys.spotify);
 var spotify = require('spotify');
-
 var request = require('request');
+
 
 
 var getArtistNames = function (artist) {
@@ -18,7 +19,7 @@ var getMeSpotify = function (songName) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-
+        console.log(data);
         var songs = data.tracks.items;
         for (var i = 0; i < songs.length; i++) {
             console.log(i);
@@ -37,11 +38,37 @@ var getMeMovie = function (movieName) {
     request('http://www.omdbapi.com/?apikey=896476c8&t=' + movieName + '&y=&plot=short&r=json', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log('error:', error); // Print the error if one occurred
-            console.log('body:', body); // Print the HTML for the Google homepage.
+            
+            var jsonData = JSON.parse(body);
+
+            console.log('Title: ' + jsonData.Title);
+            console.log('Year: ' + jsonData.Year);
+            console.log('Rated: ' + jsonData.Rated);
+            console.log('IMDB Rating: ' + jsonData.imdbRating);
+            console.log('Country: ' + jsonData.Country);
+            console.log('Language: ' + jsonData.Language);
+            console.log('Plot: ' + jsonData.Plot)
+            console.log('Actors: ' + jsonData.Actors)
+            console.log('Rotten Tomatoes Rating: ' + jsonData.tomatoRating);
+            console.log('Rotten Tomatoes URL: ' + jsonData.tomatoURL);
         }
     })
 }
 
+var doRandom = function() {
+
+fs.readFile('random.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+
+    var dataArr = data.split(',');
+    if (dataArr.length == 2) {
+        pick(dataArr[0], dataArr[1]);
+    } else if (dataArr.length ==1) {
+        pick(dataArr[0]);
+    }
+
+  });
+}
 
 var pick = function (caseData, functionData) {
     switch (caseData) {
@@ -50,6 +77,9 @@ var pick = function (caseData, functionData) {
             break;
         case 'movie-this':
             getMeMovie(functionData);
+        case 'do-what-it-says':
+            doRandom();
+            break;
         default:
             console.log('LIRI does not know that');
     }
